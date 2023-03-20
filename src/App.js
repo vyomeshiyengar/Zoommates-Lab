d6a9b4dcc436dbb529e11097cddd772b9e760f2e
André Larocque <a3larocq@uwaterloo.ca>
2023-03-20 09:58:24 -0400

    all

--- src/App.js
+++ src/App.js
@@ -1,4 +1,17 @@
+import {Provider} from 'react-redux';
+import store from './store';
+
+  import RestaurantScreen from './components/RestaurantScreen';
 export default function App() {
-  return <div>Hello, world.</div>;
-  }
+return <div>Hello, world.</div>;
+return (
+<Provider store={store}>
+
+<RestaurantScreen />
+</Provider>
+
+);
+}
+
+
   
---
+++ src/store/reducers.js
@@ -0,0 +1,3 @@
+import {combineReducers} from 'redux';
+import restaurants from './restaurants/reducers';
+export default combineReducers({restaurants});
